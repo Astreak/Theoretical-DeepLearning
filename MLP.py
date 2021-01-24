777b49b87ae83067f4d6573ea6f0f36af7944d18
praj000 <parajbhattacharjee123@gmail.com>
2021-01-24 22:53:52 +0530

    MLP sigmoid neuron

--- MLP.py
+++ MLP.py
@@ -4,8 +4,24 @@ import random
 import math
 
 class MLP(object):
-        def __init__(self,data):
-                self.data=data
+        def __init__(self,a,**k):
+                self.a=a;
+                self.k=k;
+                self.w=np.random.uniform(-0.1,0.1,(a.shape[0],1))
+                self.eta=0.13
+        def helper(self,x):
+                S=0
+                for i in x:
+                        S+=i
+                return S;
+        def sigmoid(self):
+                return 1.0/(1.0+np.exp(-np.dot(self.w.T,self.a)))
+        def P_sigmoid(self):
+                return self.sigmoid*(1-self.sigmoid(self.a))
+
 
 if __name__=="__main__":
-        pass
+        L=np.array([[0,0],[0,1],[1,0],[1,1]])
+        p=MLP(L.T)
+        
+
