69d4f2c08cb2b9498d2102593f22a305aba6aeca
praj000 <parajbhattacharjee123@gmail.com>
2021-01-24 13:36:59 +0530

    Learning MLP

---
+++ MLP.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import random
+import math
+
+class MLP(object):
+        def __init__(self,data):
+                self.data=data
+
+if __name__=="__main__":
+        pass
