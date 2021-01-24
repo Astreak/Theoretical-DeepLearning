4dd1c54c4d0705a4459fb7fd5c4fefb11961954e
praj000 <parajbhattacharjee123@gmail.com>
2021-01-25 00:32:53 +0530

    Sigmoid Neuron

--- MLP.py
+++ MLP.py
@@ -6,42 +6,47 @@ import matplotlib.pyplot as plt
 
 class SigmoidNeuron:
         def __init__(self,a):
-                self.a=np.array(a)
-                self.eta=0.29
-                self.w=np.random.uniform(-0.1,0.1,(len(a),))
-                self.b=np.random.uniform(-0.1,0.1,(1,))
+                self.a=np.array(a).T
+                self.eta=0.4
+                self.w=np.random.uniform(-0.1,0.1,(self.a.shape[0],1))
+                self.b=np.random.uniform(-0.1,0.1,(1,1))
         def sigmoid(self,x):
                 return 1.0/(1.0+np.exp(-1*(np.dot(self.w.T,self.a))))
         def p_sigmoid(self,x):
                 return self.sigmoid(x)*(1-self.sigmoid(x))
         def gradient_descent(self,y):
-                e=1000
+                y=np.array(y).T
+                e=100
                 P=[]
                 for i in range(e):
                         pred=self.sigmoid(np.dot(self.w.T,self.a)+self.b)
-                        error=0.5*(pred-y)**2;
-                        P.append(error)
-                        p_error=(pred-y)
-                        temp=p_error*self.p_sigmoid(pred)*self.a
-                        h=p_error*self.p_sigmoid(pred)
-                        self.w=self.w-self.eta*temp
-                        self.b=self.b-self.eta*h
-                # plt.plot([i for i in range(e)],P,c='r')
-                # plt.show()
+                        error=0.5*(pred-y)**2
+                        P.append(np.sum(error,axis=1))
+                        p_error=pred-y
+                        temp=p_error*self.p_sigmoid(np.dot(self.w.T,self.a)+self.b)
+                        dw=np.dot(self.a,temp.T)
+                        db=np.sum(temp,axis=1)
+                        self.w=self.w-self.eta*dw
+                        self.b=self.b-self.eta*db
+                        
+                        
+                        
+                plt.plot([i for i in range(e)],P,c='r')
+                plt.show()
                 # for i in P:
                 #         print(i)
         def evaluate(self,x):
-                x=np.array(x)
+                x=np.array(x).T
                 return self.sigmoid(np.dot(self.w.T,x)+self.b)
         
                 
                 
                 
 if __name__=="__main__":
-        L=[0.11,0.53]
+        L=[[0,0],[0,1],[1,0],[1,1]]
         p=SigmoidNeuron(L)
         print("Before gradient descent: ")
-        print(p.evaluate([0.10,0.51]))
-        p.gradient_descent(1)
+        print(p.evaluate([[1,1]]).T)
+        p.gradient_descent([[0],[0],[0],[1]])
         print("Prediction after GD")
-        print(p.evaluate([0.10,0.51]))
+        print(p.evaluate([[1,1]]).T)
